4fc37139d2170f4af6cf9226671a153bb9e0f157
Maruf <marufzoiroff@gmail.com>
2022-04-02 15:29:43 +0500

    created fake dll

---
+++ FakeDLL/Encryptor.cs
@@ -0,0 +1,14 @@
+﻿using CustomAttributes;
+using System;
+
+namespace FakeDLL
+{
+    [Author("Maruf", version = 1)]
+    public class Encryptor
+    {
+        public void Encrypt()
+        {
+
+        }
+    }
+}
