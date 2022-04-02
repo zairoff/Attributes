1acc034048c1def8872cb7060fc907b33786020b
Maruf <marufzoiroff@gmail.com>
2022-04-02 15:27:34 +0500

    created custom attribute

---
+++ CustomAttributes/AuthorAttribute .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)
+]
+    public class AuthorAttribute : Attribute
+    {
+        private readonly string name;
+        public double version;
+
+        public AuthorAttribute(string name)
+        {
+            this.name = name;
+            version = 1.0;
+        }
+    }
+}
--- CustomAttributes/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CustomAttributes
-{
-    public class Class1
-    {
-    }
-}
